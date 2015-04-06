a8ec355f54b7a4ede7dfc05cbd15f04db888cb36
schneidan <dan@scrollwright.com>
2015-04-05 22:14:03 -0600

    More 404.php fix

--- 404.php
+++ 404.php
@@ -31,7 +31,12 @@
                         </div><!-- .entry-content -->
                     </article><!-- #post-0 -->
             
-				<?php reactor_inner_content_after(); ?>
+				<div id="sidebar-2" class="sidebar large-12 small-12 columns" role="complementary">
+                    <div id="related_widget-2" class="widget related_widget">
+                        <h4 class="widget-title">Related articles</h4>
+                        <?php global $related; echo $related->show( '2929' ); ?>
+                    </div>
+                </div>
                 
                 </div><!-- .columns -->
                 
